8c6b08ed3bbb7c11c0c576b2c495415038d5f3be
Björn Martensen <bjoern.martensen@gmail.com>
2014-09-09 16:05:07 +0200

    cleanup

--- src/filters.php
+++ src/filters.php
@@ -1,6 +1,11 @@
 <?php
 
-Route::filter('multi-tenancy.auth', function () {
+use Euw\MultiTenancy\Exceptions\TenantIsNotActiveException;
+use Euw\MultiTenancy\Exceptions\TenantNotFoundException;
+use Euw\MultiTenancy\Modules\Tenants\Models\Tenant;
+
+Route::filter('multi-tenancy.auth', function ()
+{
     $context = App::make('Euw\MultiTenancy\Contexts\Context');
 
     if (Auth::guest()) return Redirect::guest('login');
@@ -8,19 +13,22 @@ Route::filter('multi-tenancy.auth', function () {
     $context->set(Auth::user());
 });
 
-Route::filter('multi-tenancy.selectTenant', function () {
+Route::filter('multi-tenancy.selectTenant', function ()
+{
     $server = explode('.', Request::server('HTTP_HOST'));
 
     if ( count($server) == 3 ) {
         $subdomain = $server[0];
 
-        $tenant = Euw\MultiTenancy\Modules\Tenants\Models\Tenant::where('subdomain', '=', $subdomain)->first();
+        $tenant = Tenant::where('subdomain', '=', $subdomain)->first();
+
+        if (!$tenant->active) throw new TenantIsNotActiveException;
 
         if ($tenant) {
             $context = App::make('Euw\MultiTenancy\Contexts\Context');
             $context->set($tenant);
         } else {
-            return Redirect::to(Request::secure() ? 'https://' : 'http://' . 'www.' . Config::get('app.domain'));
+            throw new TenantNotFoundException;
         }
     }
 
